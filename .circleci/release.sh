8550f6c320527f64d6407f10a403c4451700d425
Tristan Burch <tristan@librato.com>
2018-01-26 12:13:55 -0700

    run with profile during release

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 mvn -s .circleci/m2/settings.xml release:prepare \
+    -Prelease \
     --batch-mode
 
 
 mvn -s .circleci/m2/settings.xml release:perform \
+    -Prelease \
     --batch-mode
