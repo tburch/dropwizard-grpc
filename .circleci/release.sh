bd55fda1f7eb9af2a373d781c599e8c13b8b1b7c
Tristan Burch <tristan@librato.com>
2018-01-26 11:10:40 -0700

    use an `_`

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-mvn -Dgpg.passphrase=${env.GPG.PASSPHRASE} \
+mvn -Dgpg.passphrase=${env.GPG_PASSPHRASE} \
     -s .circleci/m2/settings.xml release:prepare \
     --batch-mode
 
 
-mvn -Dgpg.passphrase=${env.GPG.PASSPHRASE} \
+mvn -Dgpg.passphrase=${env.GPG_PASSPHRASE} \
     -s .circleci/m2/settings.xml release:perform \
     --batch-mode
