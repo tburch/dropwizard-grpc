333364daa2e5de6cdc2f2fc98d618407185a195d
Tristan Burch <tristan@librato.com>
2018-01-26 10:55:08 -0700

    Switch to CircleCI and try to automatically release from master

---
+++ .circleci/release.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mvn -Dgpg.passphrase=${env.GPG.PASSPHRASE} \
+    -s .circleci/m2/settings.xml release:prepare \
+    --batch-mode
+
+
+mvn -Dgpg.passphrase=${env.GPG.PASSPHRASE} \
+    -s .circleci/m2/settings.xml release:perform \
+    --batch-mode \
+    -DlocalCheckout=true
